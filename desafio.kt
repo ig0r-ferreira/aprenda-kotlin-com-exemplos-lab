1034ac63959087438571a1b64ac27ce763e02d04
Igor Ferreira <iferreira.n01@gmail.com>
2024-01-17 19:41:20 -0300

    feat: adiciona solução do desafio

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,84 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
+import java.util.UUID
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
+enum class Status {ABERTO, CANCELADO, FECHADO }
+enum class Papel {PADRAO, ALUNO, INSTRUTOR}
 
-class Usuario
+data class Pessoa(val nome: String, val cpf: String, var papeis: MutableList<Papel> = mutableListOf<Papel>()){
+    init {
+        papeis.add(Papel.PADRAO)
+    }
+}
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val nivel: Nivel, val duracaoEmHoras: Int = 1, var instrutor: Pessoa)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, var nivel: Nivel, var conteudos: List<ConteudoEducacional>)
 
-    val inscritos = mutableListOf<Usuario>()
+data class Contrato(val matricula: UUID, val pessoa: Pessoa, val formacao: Formacao, var status: Status = Status.ABERTO)
+
+class InstituicaoDeEnsino(var nome: String) {
+    var contratos = mutableListOf<Contrato>()
+    var formacoes = mutableListOf<Formacao>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun adicionarFormacao(formacao: Formacao) {
+        formacoes.add(formacao)
+    }
+    
+    fun matricular(pessoa: Pessoa, formacao: Formacao): UUID {
+        if (formacao !in formacoes){
+            throw IllegalArgumentException("A $nome não contempla a formação \"${formacao.nome}\".")
+        }
+        if (contratos.any{
+        	contrato -> contrato.pessoa.cpf == pessoa.cpf && 
+            contrato.formacao == formacao && 
+            contrato.status == Status.ABERTO}
+       	){
+            throw IllegalArgumentException(
+                "Já existe uma matrícula do aluno ${pessoa.nome} " +
+                "para a formação \"${formacao.nome}\"."
+            )
+        }
+        
+		val matricula: UUID = UUID.randomUUID()
+        pessoa.papeis.add(Papel.ALUNO)
+        contratos.add(Contrato(matricula, pessoa, formacao))
+        
+        return matricula
+    }
+    
+    fun cancelarMatricula(matricula: UUID){
+    	val contrato = contratos.find{contrato -> contrato.matricula == matricula}
+       	if (contrato?.status == Status.ABERTO) {
+            contrato.status = Status.CANCELADO
+        }
+    }
+    
+    fun concluirFormacao(matricula: UUID){
+        val contrato = contratos.find{contrato -> contrato.matricula == matricula}
+       	if (contrato?.status == Status.ABERTO) {
+            contrato.status = Status.FECHADO
+        }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val venilton = Pessoa("Venilton FalvoJr", "00000000000", mutableListOf(Papel.INSTRUTOR))
+    val igor = Pessoa("Igor", "11111111111")
+    
+   	val formacaoKotlin = Formacao(
+        nome="Kotlin para Back-end",
+        nivel=Nivel.INTERMEDIARIO,
+        conteudos=mutableListOf(
+            ConteudoEducacional("Lógica de programação com Kotlin", Nivel.BASICO, 40, venilton),
+            ConteudoEducacional("Padrões de projeto em Kotlin", Nivel.INTERMEDIARIO, 20, venilton),
+            ConteudoEducacional("Banco de Dados SQL e NoSQL", Nivel.INTERMEDIARIO, 20, venilton),
+            ConteudoEducacional("Kotlin no Back-end com Spring Boot 3", Nivel.INTERMEDIARIO, 30, venilton)
+        )
+    )
+    
+    val dio = InstituicaoDeEnsino("DIO")
+    dio.adicionarFormacao(formacaoKotlin)
+    
+    val matricula = dio.matricular(igor, formacaoKotlin)
+    dio.concluirFormacao(matricula)
 }
